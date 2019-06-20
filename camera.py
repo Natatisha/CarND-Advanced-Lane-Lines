08ac85eedcc98453faadd38043ff374aae77cd12
Natatisha <natatisha94@gmail.com>
2019-06-20 10:40:59 -0700

    finish camera calibration

--- camera.py
+++ camera.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import pickle
-import matplotlib.pyplot as plt
 import os
 
 
@@ -26,13 +25,21 @@ def extract_obj_points(img_path, nx, ny, out_folder='cb_corners'):
     return pattern_found, objp, corners
 
 
-def calibrate_camera(objpoints, imgpoints, img_size):
+def calibrate_camera(objpoints, imgpoints, img_size, out_folder='camera_cal', file_name='calibration_data'):
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
     # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-    dist_pickle = {}
-    dist_pickle["mtx"] = mtx
-    dist_pickle["dist"] = dist
-    pickle.dump(dist_pickle, open("camera_cal/wide_dist_pickle.p", "wb"))
+    dist_pickle = {"mtx": mtx, "dist": dist}
+    pickle.dump(dist_pickle, open(out_folder + "/" + file_name + ".p", "wb"))
 
     return ret, mtx, dist, rvecs, tvecs
+
+
+def load_calibration_results(file_path="camera_cal/calibration_data.p"):
+    dist_pickle = pickle.load(open(file_path, "rb"))
+    return dist_pickle["mtx"], dist_pickle["dist"]
+
+
+def undistort(img, matrix, distortion, rgb=True):
+    undist = cv2.undistort(img, matrix, distortion, None, matrix)
+    return undist if rgb else cv2.cvtColor(undist, cv2.COLOR_BGR2RGB)
